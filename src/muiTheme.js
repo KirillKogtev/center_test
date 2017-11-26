ea591ba2522925aa116b75ada5a0c92f63ac9f93
Kirill Kogtev <kirillkogtev95@gmail.com>
2017-11-26 14:07:22 +0300

    add style page

---
+++ src/muiTheme.js
@@ -0,0 +1,23 @@
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+export default getMuiTheme({
+    palette: {
+        textColor: '#000',
+        secondaryTextColor: '#fff',
+        alternateTextColor: '#fff',
+        accent1Color: '#95812b',
+        accent2Color: '#95812b',
+        accent3Color: '#95812b',
+        canvasColor: '#fff',
+        borderColor: '#000',
+        disabledColor: '#fff',
+        pickerHeaderColor: '#fff',
+        clockCircleColor: '#fff',
+    },
+    toolbar: {
+        backgroundColor: '#F9D247',
+    },
+    toolbar_title: {
+        fontSize: 12,
+    },
+});
---
+++ src/styles.js
@@ -0,0 +1,15 @@
+
+export default {
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        marginTop: '10px',
+    },
+    svgIcon: {
+        width: 100,
+    },
+    toolbarTitle: {
+        fontSize: 12,
+    },
+}
